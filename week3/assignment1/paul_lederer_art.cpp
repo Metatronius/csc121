7ebb1b728c5631603232f92bafe317830159a5d8
Paul E Lederer <plederer93@gmail.com>
2019-01-29 14:36:47 -0600

    moved art to the appropriate directory

--- week3/assignment1/paul_lederer_art.cpp
+++ week3/assignment1/paul_lederer_art.cpp
@@ -1,25 +1,25 @@
-#include "stdio.h"
-// #include <iostream>
+#include <iostream>
 // #include <string>
 // using std::cout;
 int main()
 {
+    //because these are string constants we can initate them as an array of character pointers and not have to use std::string
     char s1[] = "*   *   *   *   *   *",
          s2[] = "  *   *   *   *   *  ",
          s3[] = "_____________________________\n",
          s4[] = "__________________________________________________\n";
-    // cout << s4 << s1 << s3 << s2 << s3;
-    // cout << s1 << s3 << s2 << s3;
-    // cout << s1 << s3 << s2 << s3;
-    // cout << s1 << s3 << s2 << s3;
-    // cout << s4 << s4 << s4 << s4 << s4;
+    //cout << s4 << s1 << s3 << s2 << s3;
     printf("%s%s%s%s%s", s4, s1, s3, s2, s3);
+    //print pattern one 3 times
     for (char i = 0; i < 3; i++)
     {
+        //cout << s1 << s3 << s2 << s3;
         printf("%s%s%s%s", s1, s3, s2, s3);
     }
+    //print pattern two 3 times
     for (char i = 0; i < 5; i++)
     {
+        //cout << s4
         printf("%s", s4);
     }
     return 0;
