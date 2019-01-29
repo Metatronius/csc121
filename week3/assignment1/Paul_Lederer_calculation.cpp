3ee9fe151ab5a4c7bd16e409b7ab080cc57d8ca1
Paul E Lederer <plederer93@gmail.com>
2019-01-29 14:39:10 -0600

    added comments to calculation

--- week3/assignment1/Paul_Lederer_calculation.cpp
+++ week3/assignment1/Paul_Lederer_calculation.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 int main()
 {
-    double radius = 11; //centimeters
-    const double PI = 3.14159265358979323846;
+    double radius = 11;                       //centimeters
+    const double PI = 3.14159265358979323846; //more exact value of pi
     double sphere_volume = (4 / 3) * PI * (radius * radius * radius);
-    double surface_area = 4 * PI * radius * radius;
-    printf("Volume = %f Area = %f", sphere_volume, surface_area);
+    double surface_area = 4 * PI * radius * radius; //the correct equation for the surface area of a sphere
     // std::cout << "Volume = " << sphere_volume << " Area = " << surface_area;
+    printf("Volume = %f Area = %f", sphere_volume, surface_area);
     return 0;
 }
