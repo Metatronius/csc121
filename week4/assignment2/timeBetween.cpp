7c4d33e7a214ebbe64c2517b289e579518fa1786
Paul E Lederer <plederer93@gmail.com>
2019-02-06 11:57:44 -0600

    added comments to timeBetween

--- week4/assignment2/timeBetween.cpp
+++ week4/assignment2/timeBetween.cpp
@@ -1,13 +1,15 @@
-#include <iostream>
-#include <math.h>
-using namespace std;
+#include <iostream> //cout, cin
+using std::cin;
+using std::cout;
 int main()
 {
-    int time1, time2, timeBetween, hoursBetween, minutesBetween;
+    int time1, time2, timeBetween, hoursBetween, minutesBetween; //Variable declaration
     cout << "Please enter the first time: ";
-    cin >> time1;
+    cin >> time1; //receives the first time
     cout << "Please enter the second time: ";
-    cin >> time2;
-    timeBetween = abs(time1 - time2);
-    hoursBetween timeBetween / 100;
+    cin >> time2;                                                          //receives the second time
+    timeBetween = abs(time1 - time2);                                      //find the absolute difference between the times.
+    hoursBetween = timeBetween / 100;                                      //calculates difference in hours
+    minutesBetween = timeBetween % 100;                                    //calculates difference in minutes
+    cout << hoursBetween << " hours and " << minutesBetween << " minutes"; //displays output
 }
