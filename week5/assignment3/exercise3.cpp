4cd023882428cb617a535c3f01fa207346a6d55e
Paul E Lederer <plederer93@gmail.com>
2019-02-20 22:51:38 -0600

    added Martin's work

---
+++ week5/assignment3/exercise3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+// #include <string>
+using namespace std;
+
+int main()
+{
+    string grade;
+    cout << "Enter your letter grade: ";
+    cin >> grade;
+    char letter = grade[0];
+    char plusOrMinus = grade[1];
+    float gpa = 0;
+
+    switch (letter)
+    {
+    case 'A':
+    case 'a':
+        gpa = 4;
+        break;
+    case 'B':
+    case 'b':
+        gpa = 3;
+        break;
+    case 'C':
+    case 'c':
+        gpa = 2;
+        break;
+    case 'D':
+    case 'd':
+        gpa = 1;
+    }
+    if (gpa != 0)
+    {
+        if (plusOrMinus == '+' && letter != 'A')
+        {
+            gpa += .3;
+        }
+        else if (plusOrMinus == '-')
+        {
+            gpa -= .3;
+        }
+    }
+
+    cout << "The numeric value is " << gpa;
+
+    return 0;
+}
